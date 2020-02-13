695b03f91dfbefaa86ce6c336f2af702f75d28a6
J. S. Oishi <jsoishi@gmail.com>
2020-02-13 10:30:24 -0500

    Fixed plotting script

--- python/ellipse_test/plot_data.py
+++ python/ellipse_test/plot_data.py
@@ -28,7 +28,7 @@ def main(filename, start, count, output):
     title_func = lambda sim_time: 't = {:.3f}'.format(sim_time)
     savename_func = lambda write: 'write_{:06}.png'.format(write)
     # Layout
-    nrows, ncols = 1, 1
+    nrows, ncols = 2, 2
     image = plot_tools.Box(2*np.pi, 2*np.pi)
     pad = plot_tools.Frame(0.2, 0.2, 0.1, 0.1)
     margin = plot_tools.Frame(0.3, 0.2, 0.1, 0.1)
